fd6b0e340d213ea0a679e6301f23823ad6d74ca1
Tiberius Mairura <tiberiusmairura.dev@gmail.com>
2022-10-03 02:04:57 +0300

    commit tree 5045e26a945b30c80f1fb1c8ddd1f2635c7c13ce

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,19 @@
 class ItemsController < ApplicationController
+
+  wrap_parameters format: []
+
+  rescue_from ActiveRecord::RecordNotFound, :with => :render_not_found_response
+
+  def index
+    items = Item.all.order(price: :asc)
+    render json: items.as_json(
+      include: {
+        user: {except: [:created_at, :updated_at]}
+        },
+        except: [:created_at, :updated_at, :user_id]
+      ),
+    status: :ok
+  end
+
 end
+
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+
+  def show
+    user = find_user
+    render json: user.as_json(
+      include: {
+        items: { except: [:created_at, :updated_at, :user_id]}
+      },
+      except: [:created_at, :updated_at, :user_id]
+    ), status: :ok
+  end
+
+  private
+
+  def find_user
+    User.find(params[:id])
+  end
+
+  def render_not_found_response(invalid)
+    render json: {errors: invalid.redord.errors.full_messages}
+  end
 end
